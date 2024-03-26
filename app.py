c0917a3c50f007ac112e8913516dd99d43905ae6
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 13:44:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,21 @@ if uploaded_file is not None:
     img_bytes = uploaded_file.read()
     nparr = np.frombuffer(img_bytes, np.uint8)
     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-      
-
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text=pytesseract.image_to_string(img_rgb)
     st.write(text) 
+    
+      if st.button("Generar Audio en Coreano"):
+        result, output_text = text_to_speech(text)
+        audio_file_path = f"temp/{result}.mp3"
+        audio_file = open(audio_file_path, "rb")
+        audio_bytes = audio_file.read()
+        st.markdown("## Reproducir Audio en Coreano:")
+        st.audio(audio_bytes, format="audio/mp3")
+
+        st.markdown(f"## Texto traducido al coreano:")
+        st.write(output_text)
 
 def text_to_speech(text):
     translator = Translator()
@@ -42,14 +50,5 @@ def text_to_speech(text):
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, trans_text
     
-    if st.button("Generar Audio en Coreano"):
-        result, output_text = text_to_speech(text)
-        audio_file_path = f"temp/{result}.mp3"
-        audio_file = open(audio_file_path, "rb")
-        audio_bytes = audio_file.read()
-        st.markdown("## Reproducir Audio en Coreano:")
-        st.audio(audio_bytes, format="audio/mp3")
-
-        st.markdown(f"## Texto traducido al coreano:")
-        st.write(output_text)
+  
     
