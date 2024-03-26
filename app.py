4a0e3073fd69b04cefb9a9ef76c644b6c8deebd9
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 13:41:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,6 @@ def text_to_speech(text):
     translation = translator.translate(text, dest='ko')  # Traducir al coreano
     trans_text = translation.text
 
-    # Es posible que necesites procesar el texto coreano aquí para que se muestre correctamente en letras coreanas
-
     tts = gTTS(trans_text, lang='ko', slow=False)  # Generar audio en coreano
     try:
         my_file_name = text[0:20]
@@ -43,4 +41,23 @@ def text_to_speech(text):
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, trans_text
+
+if uploaded_file is not None:
+    img_bytes = uploaded_file.read()
+    nparr = np.frombuffer(img_bytes, np.uint8)
+    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text = pytesseract.image_to_string(img_rgb)
+    st.write(text) 
+    
+    if st.button("Generar Audio en Coreano"):
+        result, output_text = text_to_speech(text)
+        audio_file_path = f"temp/{result}.mp3"
+        audio_file = open(audio_file_path, "rb")
+        audio_bytes = audio_file.read()
+        st.markdown("## Reproducir Audio en Coreano:")
+        st.audio(audio_bytes, format="audio/mp3")
+
+        st.markdown(f"## Texto traducido al coreano:")
+        st.write(output_text)
     
